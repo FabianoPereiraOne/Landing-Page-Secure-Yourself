2e43d73617690e73bc03ed12939f80e85734e626
Fabiano Pereira <fabianop971@gmail.com>
2025-03-16 13:29:37 -0300

    Feat: add link checkout

--- src/components/LinkBuy/index.tsx
+++ src/components/LinkBuy/index.tsx
@@ -4,7 +4,7 @@ import { FiArrowRight } from "react-icons/fi"
 export const LinkBuy = ({ classAdt }: { classAdt?: string }) => {
   return (
     <Link
-      href='/'
+      href='https://terapeutajosemaria.com.br/blog/downloads/ebook-nao-seja-a-proxima-vitima/'
       className={`${
         classAdt ?? ""
       } max-w-[220px] bg-red-500 px-6 py-3 text-white text-md font-semibold uppercase rounded-[6px] flex items-center justify-center gap-2 hover:bg-red-600 transition-all ease-in`}
